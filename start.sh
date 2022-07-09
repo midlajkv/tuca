f368b894ae3776a150a808751cfd1d4b7716a719
Amal nath <96761219+Masterrockiei@users.noreply.github.com>
2022-07-09 21:49:28 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Masterrockiei/tuca.git /Sakura
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /tuca
+fi
+cd /tuca
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
